b788e08406e93c5dece80d459a5840a445d9dceb
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-11-02 20:41:42 -0700

    safe point

--- 14-MVC/02-Pets-or-animals/Unsolved/server.js
+++ 14-MVC/02-Pets-or-animals/Unsolved/server.js
@@ -1,52 +1,59 @@
 // Dependencies
 // =============================================================
-const express = require('express');
-const exphbs = require('express-handlebars');
+const express = require("express");
+const exphbs = require("express-handlebars");
 
 // Sets up the Express App
 // =============================================================
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set Handlebars as the default template engine
-//
-// YOUR CODE HERE
-//
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
 // Data
 // =============================================================
 const animals = [
   {
-    animalType: 'dog',
+    animalType: "dog",
     pet: true,
-    fierceness: 4
+    fierceness: 4,
   },
   {
-    animalType: 'cat',
+    animalType: "cat",
     pet: true,
-    fierceness: 6
+    fierceness: 6,
   },
   {
-    animalType: 'giraffe',
+    animalType: "giraffe",
     pet: false,
-    fierceness: 4
+    fierceness: 4,
   },
   {
-    animalType: 'zebra',
+    animalType: "zebra",
     pet: false,
-    fierceness: 8
+    fierceness: 8,
   },
   {
-    animalType: 'lion',
+    animalType: "lion",
     pet: false,
-    fierceness: 10
-  }
+    fierceness: 10,
+  },
 ];
 
 // Routes
 // =============================================================
 
-app.get('/all-pets', (req, res) => {
+app.get("/all-pets", (req, res) => {
+  const data = {
+    animals: [],
+  };
+  for (let i = 0; i < animals.length; i++) {
+    if (animals[i].pet) {
+      data.animals.push(animals[i]);
+    }
+  }
+  res.render("index", data);
   // Loop through the animals, and send those that are pets to the `index.handlebars` file.
   // Hint: Handlebars requires an object to be sent to the `index.handlebars` file, not an array!
   //
@@ -54,7 +61,17 @@ app.get('/all-pets', (req, res) => {
   //
 });
 
-app.get('/all-non-pets', (req, res) => {
+app.get("/all-non-pets", (req, res) => {
+  const data = {
+    animals: [],
+  };
+  for (let i = 0; i < animals.length; i++) {
+    if (!animals[i].pet) {
+      data.animals.push(animals[i]);
+    }
+  }
+  res.render("index", data);
+
   // Loop through the animals, and send those that are NOT pets to the `index.handlebars` file.
   // Hint: Handlebars requires an object to be sent to the `index.handlebars` file, not an array!
   //
@@ -65,5 +82,5 @@ app.get('/all-non-pets', (req, res) => {
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, () => {
-  console.log('App listening on PORT ' + PORT);
+  console.log("App listening on PORT " + PORT);
 });
