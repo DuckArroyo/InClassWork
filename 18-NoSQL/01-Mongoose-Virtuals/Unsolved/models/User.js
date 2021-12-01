1f5656eadc0ab005459cc833e97445be00fe3a85
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-11-30 21:04:36 -0800

    update

--- 18-NoSQL/01-Mongoose-Virtuals/Unsolved/models/User.js
+++ 18-NoSQL/01-Mongoose-Virtuals/Unsolved/models/User.js
@@ -1,36 +1,42 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const UserSchema = new Schema(
   {
     firstName: {
       type: String,
       trim: true,
-      required: 'First Name is Required'
+      required: "First Name is Required",
     },
 
     lastName: {
       type: String,
       trim: true,
-      required: 'Last Name is Required'
+      required: "Last Name is Required",
     },
 
     password: {
       type: String,
       trim: true,
-      required: 'Password is Required',
-      validate: [({ length }) => length >= 6, 'Password should be longer.']
+      required: "Password is Required",
+      validate: [({ length }) => length >= 6, "Password should be longer."],
     },
 
     email: {
       type: String,
       unique: true,
-      match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+      match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
     },
 
     userCreated: {
       type: Date,
-      default: Date.now
-    }
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
   }
   // Set the `toJSON` schema option to use virtuals
   // Set the `id` as false
@@ -40,8 +46,11 @@ const UserSchema = new Schema(
 
 // Create a virtual property `username` that's computed from the front part of `email` before the `@` symbol.
 // YOUR CODE HERE
-//
+// Cannot be a fat arrow or it will not know the reference to the schema above.
+UserSchema.virtual("username").get(function () {
+  return this.email.slice(0, this.email.indexOf("@"));
+});
 
-const User = model('User', UserSchema);
+const User = model("User", UserSchema);
 
 module.exports = User;
