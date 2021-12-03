3e0f15ff3123c21b01fc812bc0bb6b9a2cbc4a6a
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-12-02 19:24:21 -0800

    class execersice

--- 18-NoSQL/03-Mongoose-Schema/Unsolved/models/User.js
+++ 18-NoSQL/03-Mongoose-Schema/Unsolved/models/User.js
@@ -1,11 +1,32 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const UserSchema = new Schema({
   // Add these four attributes to your schema: username, password, email, userCreated
   // YOUR CODE HERE
   //
+  userName: {
+    type: String,
+    trim: true,
+    require: "User Name is required",
+  },
+  password: {
+    type: String,
+    trim: true,
+    require: true,
+    unique: true,
+    validate: [({ length }) => length >= 6, "Password should be longer."],
+  },
+  email: {
+    type: String,
+    required: "You must enter an email!",
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address!"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const User = model('User', UserSchema);
+const User = model("User", UserSchema);
 
 module.exports = User;
