707e69e06a0dfa9a93edee17e6d1adc420203918
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-12-14 20:40:08 -0800

    in class work

---
+++ 20-React/01-Component-Map/01-Component-Map/inclass/src/Button.js
@@ -0,0 +1,5 @@
+function Button(props) {
+  return <button onClick={props.onClick}>Click Me!</button>;
+}
+
+export default Button;
---
+++ 20-React/01-Component-Map/01-Component-Map/inclass/src/Headers.js
@@ -0,0 +1,11 @@
+function Header(props) {
+  return (
+    <div className="App">
+      <Header title="HEllow" desription="Welcome to the first REach class" />
+
+      <Header title="Bye" />
+    </div>
+  );
+}
+
+export default Header;
