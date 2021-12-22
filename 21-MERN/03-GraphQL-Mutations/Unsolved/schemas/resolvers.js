52974f94e07d302fe1e784df4036a208e997d36b
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2021-12-21 21:09:40 -0800

    after class 12/21

--- 21-MERN/03-GraphQL-Mutations/Unsolved/schemas/resolvers.js
+++ 21-MERN/03-GraphQL-Mutations/Unsolved/schemas/resolvers.js
@@ -8,7 +8,15 @@ const resolvers = {
     book: async (parent, { title }) => {
       return Book.findOne({ title });
     }
-  }
+  },
+
+  Mutation: {
+    addBook: async (parent, args) => {
+      const book = await Book.create(args);
+      return book;
+    }
+
+  },
 };
 
 module.exports = resolvers;
--- 21-MERN/03-GraphQL-Mutations/Unsolved/server.js
+++ 21-MERN/03-GraphQL-Mutations/Unsolved/server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const app = express();
 
 const server = new ApolloServer({
