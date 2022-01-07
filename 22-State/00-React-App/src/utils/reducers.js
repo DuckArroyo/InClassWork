eda776f5efcf6a0bc2a69d332f2243f981dfff52
Rafael "Duck" Arroyo <87622570+DuckArroyo@users.noreply.github.com>
2022-01-06 19:32:05 -0800

    in class

--- 22-State/00-React-App/src/utils/reducers.js
+++ 22-State/00-React-App/src/utils/reducers.js
@@ -1,18 +1,22 @@
+// Remove all traces of useReducer
 import { useReducer } from 'react';
 import { UPDATE_BOOKS, UPDATE_CURRENT_BOOK } from './actions';
+
+// create initialState for the reducer to use
+//
 
 export const reducer = (state, action) => {
   switch (action.type) {
     case UPDATE_BOOKS:
       return {
         ...state,
-        books: [...action.books],
+        books: [...action.books]
       };
 
     case UPDATE_CURRENT_BOOK:
       return {
         ...state,
-        currentBook: { ...action.currentBook },
+        currentBook: { ...action.currentBook }
       };
     default:
       return state;
@@ -22,3 +26,5 @@ export const reducer = (state, action) => {
 export function useBookReducer(initialState) {
   return useReducer(reducer, initialState);
 }
+
+// Export the reducer
